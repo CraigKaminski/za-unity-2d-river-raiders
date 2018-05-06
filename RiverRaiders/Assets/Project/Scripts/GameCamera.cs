fed1494ad742b3b2beee4c6e43eb476e495bddd5
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-05-06 16:58:37 -0400

    Add player

---
+++ RiverRaiders/Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+    public GameObject target;
+    public float verticalOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = new Vector3(
+            0,
+            target.transform.position.y + verticalOffset,
+            transform.position.z
+        );
+	}
+}
---
+++ RiverRaiders/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float horizontalSpeed = 3f;
+    public float vertcalSpeed = 1f;
+    public float horizontalLimit = 2.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move the player.
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, vertcalSpeed);
+
+        // Keep the player within bounds.
+        if (transform.position.x > horizontalLimit)
+        {
+            transform.position = new Vector2(horizontalLimit, transform.position.y);
+        } else if (transform.position.x < -horizontalLimit)
+        {
+            transform.position = new Vector2(-horizontalLimit, transform.position.y);
+        }
+	}
+}
