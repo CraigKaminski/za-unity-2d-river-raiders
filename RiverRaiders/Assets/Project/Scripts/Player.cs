d9a69405b8dec6fbefbaa19c599a8514bde532b8
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-05-06 17:11:38 -0400

    Add ability for player to fire bullet

--- RiverRaiders/Assets/Project/Scripts/Player.cs
+++ RiverRaiders/Assets/Project/Scripts/Player.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public GameObject bulletPrefab;
     public float horizontalSpeed = 3f;
     public float vertcalSpeed = 1f;
     public float horizontalLimit = 2.8f;
+    public float bulletSpeed = 2f;
+
+    private bool fired = false;
 
 	// Use this for initialization
 	void Start () {
@@ -25,6 +29,22 @@ public class Player : MonoBehaviour {
         } else if (transform.position.x < -horizontalLimit)
         {
             transform.position = new Vector2(-horizontalLimit, transform.position.y);
+        }
+
+        if (Input.GetAxis("Fire1") == 1f)
+        {
+            if (fired == false)
+            {
+                fired = true;
+                GameObject bulletInstance = Instantiate(bulletPrefab);
+                bulletInstance.transform.SetParent(transform.parent);
+                bulletInstance.transform.position = transform.position;
+                bulletInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletSpeed);
+                Destroy(bulletInstance, 3f);
+            }
+        } else
+        {
+            fired = false;
         }
 	}
 }
